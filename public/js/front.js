d7cda52a14cd29308b799e26175f06c8493c9ebb
Anders Andersson <trezker@gmail.com>
2015-12-17 15:18:48 +0100

    Context, db connections

--- public/js/front.js
+++ public/js/front.js
@@ -1,4 +1,7 @@
 function load_front() {
+	ajax_post({'func': 'get_user'}, function(data) {
+		alert(data);
+	});
 	ajax_html('/html/front.html', function(data) {
 		$('body').html(data);
 	});
--- source/app.d
+++ source/app.d
@@ -8,92 +8,98 @@ import ddbc.common;
 import ddbc.drivers.mysqlddbc;
 import ddbc.pods;
 
+class Morzo_server {
+private:
+	DataSource datasource;
+public:
+	bool databaseSetup() {
+		try
+		{
+			if(!exists("database.json")) {
+				logInfo("Missing database.json");
+				return false;
+			}
+			string json = readText("database.json");
+			JSONValue[string] document = parseJSON(json).object;
+			JSONValue[string] settings = document["dev"].object;
+			auto host = settings["host"].str;
+			auto port = to!ushort(settings["port"].integer);
+			auto database = settings["database"].str;
+			auto user = settings["user"].str;
+			auto password = settings["password"].str;
+
+			auto driver = new MySQLDriver();
+		    auto url = MySQLDriver.generateUrl(host, port, database);
+		    auto params = MySQLDriver.setUserAndPassword(user, password);
+			datasource = new ConnectionPoolDataSourceImpl(driver, url, params);
+		}
+		catch(Exception e) {
+			logInfo(e.msg);
+		}
+	    return true;
+	}
+
+	void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error) {
+		res.render!("error.dt", req, error);
+	}
+
+	void index(HTTPServerRequest req, HTTPServerResponse res) {
+		res.render!("index.dt", req);
+	}
+
+	void ajax(HTTPServerRequest req, HTTPServerResponse res) {
+		try {
+			string func = req.json.func.to!string;
+			switch(func) {
+				case "get_logged_in_user":
+					res.writeJsonBody(false);
+					break;
+				case "get_user":
+					auto conn = datasource.getConnection();
+					scope(exit) conn.close();
+					auto stmt = conn.createStatement();
+					scope(exit) stmt.close();
+
+					auto rs = stmt.executeQuery("select id, name, pass, salt, created from user;");
+					string name;
+					while (rs.next()) {
+						name = to!string(rs.getString("name"));
+					    logInfo(name);
+					}
+					res.writeJsonBody(name);
+					break;
+				default:
+					res.writeJsonBody(false);
+					break;
+			}
+		}
+		catch(Exception e) {
+			logInfo(e.msg);
+		}
+	}
+}
+
 shared static this() {
+	auto morzo_server = new Morzo_server;
+
 	runTask({
-		if(!databaseSetup()) {
+		if(!morzo_server.databaseSetup()) {
 			logInfo("Database setup failed.");
 			exit(-1);
 		}
 	});
 	
-
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	settings.errorPageHandler = toDelegate(&errorPage);
+	settings.errorPageHandler = toDelegate(&morzo_server.errorPage);
 
 	auto router = new URLRouter;
-	router.get("/", &index);
-	router.post("/ajax", &ajax);
+	router.get("/", &morzo_server.index);
+	router.post("/ajax", &morzo_server.ajax);
 	router.get("/*", serveStaticFiles("./public/"));
 	
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
-
-bool databaseSetup() {
-	try
-	{
-		if(!exists("database.json")) {
-			logInfo("Missing database.json");
-			return false;
-		}
-		string json = readText("database.json");
-		logInfo(json);
-		JSONValue[string] document = parseJSON(json).object;
-		JSONValue[string] settings = document["dev"].object;
-		auto host = settings["host"].str;
-		auto port = to!ushort(settings["port"].integer);
-		auto database = settings["database"].str;
-		auto user = settings["user"].str;
-		auto password = settings["password"].str;
-
-		auto driver = new MySQLDriver();
-	    auto url = MySQLDriver.generateUrl(host, port, database);
-	    auto params = MySQLDriver.setUserAndPassword(user, password);
-		// create connection pool
-		DataSource ds = new ConnectionPoolDataSourceImpl(driver, url, params);
-		// creating Connection
-		auto conn = ds.getConnection();
-		scope(exit) conn.close();
-		// creating Statement
-		auto stmt = conn.createStatement();
-		scope(exit) stmt.close();
-		// reading DB
-		/*
-		auto rs = stmt.executeQuery("select id, name, pass, salt, created from user;");
-		while (rs.next())
-		    logInfo(to!string(rs.getString("name")));
-		*/
-	}
-	catch(Exception e) {
-		logInfo(e.msg);
-	}
-    return true;
-}
-
-void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error) {
-	res.render!("error.dt", req, error);
-}
-
-void index(HTTPServerRequest req, HTTPServerResponse res) {
-	res.render!("index.dt", req);
-}
-
-void ajax(HTTPServerRequest req, HTTPServerResponse res) {
-	try {
-		string func = req.json.func.to!string;
-		switch(func) {
-			case "test":
-				res.writeJsonBody(true);
-				break;
-			default:
-				res.writeJsonBody(false);
-				break;
-		}
-	}
-	catch(Exception e) {
-		logInfo(e.msg);
-	}
 }
