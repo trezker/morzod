fa4b6f50d049510c8306830643142b4a0a7a28e9
Anders Andersson <trezker@gmail.com>
2016-01-23 21:51:43 +0100

    Created base for desktop system. Added account creation and working login/logout on site.

--- public/js/front.js
+++ public/js/front.js
@@ -4,12 +4,48 @@ function load_front() {
 			ajax_html('/html/login_page.html', function(data) {
 				$('body').html(data);
 				setup_ajax_form($('form#login'), function(data) {
+					if(data == true) {
+						load_desktop();
+					}
+					console.log(data);
+				});
+				setup_ajax_form($('form#create_account'), function(data) {
 					console.log(data);
 				});
 			});
 		}
 		else {
 			//TODO: Show front page for a logged in user
+			load_desktop();
 		}
+	});
+}
+
+function load_desktop() {
+	ajax_html('/html/desktop.html', function(data) {
+		$('body').html(data);
+
+		$(".flex-container").sortable().disableSelection();
+		$("#disablesort").on("click", function(){
+			$(".flex-container").sortable( "disable" ).enableSelection();;
+		});
+		$("#enablesort").on("click", function(){
+			$(".flex-container").sortable("enable").disableSelection();;
+		});
+
+		var n = 0;
+		$("#add").on('click', function() {
+			n++;
+			$(".flex-container").append('<div class="flex-item">' + n + '</div>');
+		});
+
+		$("#logout").on('click', function() {
+			ajax_post({
+				'model': 'user', 
+				'method': 'logout'
+			}, function(data) {
+				load_front();
+			});
+		});
 	});
 }
